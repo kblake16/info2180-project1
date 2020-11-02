968e296e0f868e36b87cbbfec6ab72463d75090b
Kayla Blake <kayla.blake16@gmail.com>
2020-11-01 19:28:50 -0500

    Edit Files

--- scripts.js
+++ scripts.js
@@ -1,7 +1,32 @@
 /* Add your JavaScript to this file */
 
-"use strict";
+//"use strict";
 
 window.onload = function()
 {
+    let form = document.getElementsByTagName('form').item(0);
+
+    form.onsubmit = function(){
+        return submitEmail(form);
+    }
+
+    function submitEmail(data)
+    {
+        let email = data.email.value;
+        console.log(email);
+
+        let msg = document.querySelector('.message');
+
+        if(email)
+        {
+            msg.innerHTML = "Thank you! Your email address " + email + " as been added to our mailing list!";
+        }
+        else
+        {
+            msg.innerHTML = "Please enter a valid email address.";
+        }
+        
+        form.reset();
+        return false;
+    }
 }
